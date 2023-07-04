710d966edf346c5ec92375db11ef1cff2e0105e7
Nikita <50026919+Nubovik01@users.noreply.github.com>
2023-07-04 20:27:17 +0300

    update footer.js

--- files/js/footer.js
+++ files/js/footer.js
@@ -8,25 +8,43 @@ function codeText(text) {
   return text;
 }
 
-var footer_texts = [
-  'Cтраница слишком маленькая? Измените масштаб!',
-  'Вам тоже не нравится реклама? Извините... :(',
+var phrasesForFooter = [
   'я дед инсайд, мне 9 лет, я хочу в психокидс',
   'обними меня, мне страшно...',
   'amogus is sus',
+  'oooooo you like boys ur a boykisser',
+  'я люблю рояльчика!!<3',
   'я пообедал и мне стало уютненько',
   'iluvrldv1',
-  'трахни меня, рояль!',
   'шни шна шнапи шнапи шнапи шнап',
+  'Made in Russia w/ <3',
+  'aSBsaWtlIGJveXM=',
   'furry furry furry!',
+  'я люблю милых фембойчиков каждому бы отдался',
+  'я вас всех забаню хуесосы!, — Екатерина Мизулина',
+  'w-where are all the m-men?? i need men!',
+  '1000-7',
   'i like javascript',
+  'nothing.',
+  'so f3ck1ng psycho 🥱',
+  'мы сидим в дискордике, скинь мне ножки, сучка',
   'go kitty go!',
-  'nikita is ga~.. w-what...',
-  'подписывайтесь на тгк: @nubovikxd'
+  'you cannot escape the boykisser',
+  'нихуя что это щас было',
+  'подписывайтесь на тгк: t.me/nubovikxd',
+  'выфрщфвышфывшвышщфофвышо',
+  'мы поколение амням',
+  'donate me: qiwi.com/n/nubovik',
+  'meow^^',
+  'вам тоже не нравится реклама? :33',
+  '..o_O',
+  'кот застрял на дереве...',
+  't.me/minitypebot',
+  'more about flameout here - t.me/flameoutupdates'
 ];
 
-function footer_change_text() {
-  document.getElementById("footer-text").innerHTML = codeText(footer_texts[Math.floor(Math.random() * footer_texts.length)]);
+function footerChangeText() {
+  document.getElementById("footer-text").innerHTML = codeText(phrasesForFooter[Math.floor(Math.random() * phrasesForFooter.length)]);
 
   if (/iPhone/i.test(navigator.userAgent) && isIphone == false) {
     isIphone = true;
