8627deef55f078e52459dec5d7538588e4e08336
Nikita Akumov <50026919+nubovik01@users.noreply.github.com>
2023-11-12 01:21:11 +0300

    fix doNotTrack.js

--- files/js/doNotTrack.js
+++ files/js/doNotTrack.js
@@ -1,4 +1,4 @@
 // code by t.me/qwkrtezzz
 // file created at 11 November 2023 22:40:49 PM (UTC+3)
 
-if(userDoNotTrackMark == '1') console.log('We respect your anonymity and confidentiality, but third party services will tracking you despite the "do not track" option being enabled.');
+if(navigator.doNotTrack == '1') console.log('We respect your anonymity and confidentiality, but third party services will tracking you despite the "do not track" option being enabled.');
