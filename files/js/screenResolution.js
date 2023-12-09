85a5f73eb80c13464b43debb40971a7d5861b2c5
Nikita Akumov <50026919+nubovik01@users.noreply.github.com>
2023-12-10 00:46:22 +0300

    some fixes in css

---
+++ files/js/screenResolution.js
@@ -0,0 +1,14 @@
+// code by qwkrtezzz (github.com/nubovik01)
+// file created at 09 May 2021 00:20:29 AM (UTC+3)
+
+function getSize() {
+  console.log(screen.height, screen.width);
+};
+
+function getScreenResolution() {
+  console.log("Get screen resolution per 150 ms.");
+
+  setInterval(() => {
+    getSize();
+  }, 150);
+};
