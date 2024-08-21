eb734620abb2f19f329b3688f88e8081e062d249
Nikita Akumov <nubovik01@gmail.com>
2024-08-21 03:43:18 +0300

    Update redirecter.js

--- files/js/redirecter.js
+++ files/js/redirecter.js
@@ -3,11 +3,29 @@
 
 const platform = window.location.search.split("?r=")[1];
 const urls = {
-  "ds-serv": "https://discord.gg/EJc8UC7yhZ",
   "kladmen-rabota": "https://youtu.be/dQw4w9WgXcQ",
   screenshot: "https://nubovik01.github.io/prntsc-screamer",
+  "ds-serv": "https://discord.gg/EJc8UC7yhZ",
+  ds: "https://discord.gg/EJc8UC7yhZ",
   vk: "https://vk.com/qwkrtezzz",
-  blog: "https://t.me/nbvkxd"
+  blog: "https://t.me/nbvkxd",
+  bio: "https://t.me/nbvkbio",
+  mt: "https://monkeytype.com/profile/qwkrtezzz",
+  spotify: "https://open.spotify.com/user/tx9f1ojp5ghsdkbsrn085sn2k",
+  tt: "https://tiktok.com/@nubovik",
+  tg: "https://t.me/qwkrzbot",
+  faq: "https://t.me/qwkrzfaq",
+  gh: "https://github.com/nubovik01",
+  circus: "https://t.me/nbvkcircus",
+  pint: "https://ru.pinterest.com/qwkrtezzz",
+  twx: "https://x.com/impostornubovik",
+  redit: "https://reddit.com/user/nubovik01",
+  dnt: "https://telegra.ph/tip-me-05-10",
+  stim: "https://steamcommunity.com/id/nubovik01",
+  ymuz: "https://music.yandex.ru/users/Nubovikgg/"
 };
 
 if (platform && urls[platform]) window.location.href = urls[platform];
+
+// TODO: сделать всплывающий блок снизу экрана о том что редирект не найден
+if (!urls[platform]) console.log("There doesn't seem to be such a redirect...");
