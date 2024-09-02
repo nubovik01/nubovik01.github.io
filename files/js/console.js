9a389a2c3b00a9a9d03b427167da4d4de0cb668e
qwkrtezzz <nubovik01@gmail.com>
2024-09-02 03:52:54 +0300

    сменил https на www

--- files/js/console.js
+++ files/js/console.js
@@ -2,7 +2,7 @@
 // file created at 4 February 2024 12:26:03 PM (UTC+3)
 
 console.log("[*] %cHello! Found a bug? - t.me/qwkrzbot", "color: lightgreen;");
-console.log("[*] %csite created by qwkrtezzz (https://github.com/nubovik01)", "color: lightgreen;");
+console.log("[*] %csite created by qwkrtezzz (www.github.com/nubovik01)", "color: lightgreen;");
 
 console.log("[*] %s", navigator.userAgent);
 
