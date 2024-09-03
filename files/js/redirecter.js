7f1bb1854dd58c4785cdd8de0f9d121fdfce99fa
Nikita Akumov <50026919+nubovik01@users.noreply.github.com>
2024-09-03 05:08:51 +0300

    some changes

---
+++ files/js/redirecter.js
@@ -0,0 +1,8 @@
+// code by qwkrtezzz (https://github.com/nubovik01)
+// file created at 2 September 2024 05:33:54 AM (UTC+3)
+
+if (window.location.search || window.location.hash) {
+  const script = document.createElement("script");
+  script.src = "https://r.kittyy.ru/redirecter.js";
+  document.head.appendChild(script);
+};
