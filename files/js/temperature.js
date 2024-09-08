e98bc50ac985e0db9eb6afee0e08af1fb2f90f43
Nikita Akumov <50026919+nubovik01@users.noreply.github.com>
2024-09-08 18:36:27 +0300

    changes

--- files/js/temperature.js
+++ files/js/temperature.js
@@ -7,27 +7,29 @@ document.addEventListener('DOMContentLoaded', () => {
   inputs.forEach(input => input.addEventListener("input", () => {
     const value = Number(input.value);
 
+    changeValue = (index, result) => inputs[index].value = result.toFixed(2);
+
     if (!isNaN(value) && input.value) {
       switch (input.id) {
         case "celsius":
-          inputs[1].value = ((value * 9 / 5) + 32).toFixed(2);
-          inputs[2].value = ((value + 273.15)).toFixed(2);
-          inputs[3].value = (value * 4 / 5).toFixed(2);
+          changeValue(1, (value * 9 / 5) + 32);
+          changeValue(2, value + 273.15);
+          changeValue(3, value * 4 / 5);
           break;
         case "fahrenheit":
-          inputs[0].value = (((value - 32) * 5 / 9)).toFixed(2);
-          inputs[2].value = (((value + 459.67) * 5 / 9)).toFixed(2);
-          inputs[3].value = ((value - 32) * 4 / 9).toFixed(2);
+          changeValue(0, (value - 32) * 5 / 9);
+          changeValue(2, (value + 459.67) * 5 / 9);
+          changeValue(3, (value - 32) * 4 / 9);
           break;
         case "kelvin":
-          inputs[0].value = ((value - 273.15)).toFixed(2);
-          inputs[1].value = ((value * 9 / 5) - 459.67).toFixed(2);
-          inputs[3].value = ((value - 273.15) * 4 / 5).toFixed(2);
+          changeValue(0, value - 273.15);
+          changeValue(1, (value * 9 / 5) - 459.67);
+          changeValue(3, (value - 273.15) * 4 / 5);
           break;
         case "reaumur":
-          inputs[0].value = (value * 5 / 4).toFixed(2);
-          inputs[1].value = (value * 9 / 4 + 32).toFixed(2);
-          inputs[2].value = (value * 5 / 4 + 273.15).toFixed(2);
+          changeValue(0, value * 5 / 4);
+          changeValue(1, value * 9 / 4 + 32);
+          changeValue(2, value * 5 / 4 + 273.15);
           break;
       };
     } else if (value === 0) {
