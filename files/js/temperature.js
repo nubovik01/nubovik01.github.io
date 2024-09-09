6a7485de298cdaf1713eed7c46a0b95a2aa7dce2
Nikita Akumov <50026919+nubovik01@users.noreply.github.com>
2024-09-09 03:28:12 +0300

    fixes and changes

--- files/js/temperature.js
+++ files/js/temperature.js
@@ -1,39 +1,44 @@
 // code by qwkrtezzz (https://github.com/nubovik01)
 // file created at 8 September 2024 05:19:41 AM (UTC+3)
 
+const units = {
+  celsius: {
+    fahrenheit: value => (value * 9 / 5) + 32,
+    kelvin: value => value + 273.15,
+    reaumur: value => value * 4 / 5
+  },
+  fahrenheit: {
+    celsius: value => (value - 32) * 5 / 9,
+    kelvin: value => (value + 459.67) * 5 / 9,
+    reaumur: value => (value - 32) * 4 / 9
+  },
+  kelvin: {
+    celsius: value => value - 273.15,
+    fahrenheit: value => (value * 9 / 5) - 459.67,
+    reaumur: value => (value - 273.15) * 4 / 5
+  },
+  reaumur: {
+    celsius: value => value * 5 / 4,
+    fahrenheit: value => value * 9 / 4 + 32,
+    kelvin: value => value * 5 / 4 + 273.15
+  }
+};
+
 document.addEventListener('DOMContentLoaded', () => {
-  const inputs = document.querySelectorAll(["#celsius", "#fahrenheit", "#kelvin", "#reaumur"]);
+  const inputs = document.querySelectorAll("#celsius, #fahrenheit, #kelvin, #reaumur");
 
-  inputs.forEach(input => input.addEventListener("input", () => {
-    const value = Number(input.value);
+  inputs.forEach(pageInput => pageInput.addEventListener('input', () => {
+    pageInput.value = pageInput.value.replace(/[^0-9\-\+\.]/g, '');
+    const value = Number(pageInput.value);
 
-    changeValue = (index, result) => inputs[index].value = result.toFixed(2);
-
-    if (!isNaN(value) && input.value) {
-      switch (input.id) {
-        case "celsius":
-          changeValue(1, (value * 9 / 5) + 32);
-          changeValue(2, value + 273.15);
-          changeValue(3, value * 4 / 5);
-          break;
-        case "fahrenheit":
-          changeValue(0, (value - 32) * 5 / 9);
-          changeValue(2, (value + 459.67) * 5 / 9);
-          changeValue(3, (value - 32) * 4 / 9);
-          break;
-        case "kelvin":
-          changeValue(0, value - 273.15);
-          changeValue(1, (value * 9 / 5) - 459.67);
-          changeValue(3, (value - 273.15) * 4 / 5);
-          break;
-        case "reaumur":
-          changeValue(0, value * 5 / 4);
-          changeValue(1, value * 9 / 4 + 32);
-          changeValue(2, value * 5 / 4 + 273.15);
-          break;
-      };
-    } else if (value === 0) {
-      inputs.forEach(input => input.value = "");
+    if (!isNaN(value) && pageInput.value) {
+      inputs.forEach(userInput => {
+        if (userInput !== pageInput) {
+          userInput.value = units[pageInput.id][userInput.id](value).toFixed(2);
+        };
+      });
+    } else {
+      inputs.forEach(pageInput => pageInput.value = '');
     };
   }));
 });
