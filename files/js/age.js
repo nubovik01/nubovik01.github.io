9365c72f2e74d7ff873d49734f8c7c3d97749bff
Nikita Akumov <50026919+nubovik01@users.noreply.github.com>
2024-10-03 14:59:02 +0300

    Fix for macOS Safari and etc changes

--- files/js/age.js
+++ files/js/age.js
@@ -1,8 +1,13 @@
 // code by qwkrtezzz (https://github.com/nubovik01)
 // file created at 3 October 2024 02:00:12 AM (UTC+3)
 
-function calculateAge(birth) {// mm/dd/yyyy
-  const [birthDate, today] = [new Date(birth), new Date()];
+function convertDateFormat(date) { // dd.mm.yyyy => yyyy-mm-dd
+  const parts = date.split('.');
+  return `${parts[2]}-${parts[1]}-${parts[0]}`;
+};
+
+function calculateAge(birth) {
+  const [birthDate, today] = [new Date(convertDateFormat(birth)), new Date()];
 
   let age = today.getFullYear() - birthDate.getFullYear();
   const month = today.getMonth() - birthDate.getMonth();
