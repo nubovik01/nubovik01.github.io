649388650c7209e17d96945ec7a7fadd4a78b3bb
Nikita Akumov <50026919+nubovik01@users.noreply.github.com>
2025-02-10 06:18:03 +0300

    tests

---
+++ files/js/preloader.js
@@ -0,0 +1,18 @@
+// code by qwkrtezzz (https://github.com/nubovik01)
+// file created at 10 February 2025 06:14:31 PM (UTC+3)
+ 
+window.addEventListener('DOMContentLoaded', () => {
+  const preloader = document.createElement("div");
+  preloader.id = "preloader";
+
+  const spinner = document.createElement("div");
+  spinner.className = "spinner";
+  preloader.appendChild(spinner);
+
+  document.body.insertBefore(preloader, document.body.firstChild);
+});
+
+window.addEventListener('load', function () {
+  const preloader = document.getElementById("preloader");
+  preloader.style.display = "none";
+});
