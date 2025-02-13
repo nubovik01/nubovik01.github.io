1e9158ed3e872bb4e3959c08530239a798f6b18f
Nikita Akumov <50026919+nubovik01@users.noreply.github.com>
2025-02-13 06:36:03 +0300

    why

--- files/js/sha.js
+++ files/js/sha.js
@@ -2,14 +2,14 @@
 // file created at 17 February 2024 01:57:08 AM (UTC+3)
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const api = await fetch('https://api.github.com/repos/nubovik01/nubovik01.github.io/commits');
+  const api = await fetch("https://api.github.com/repos/nubovik01/nubovik01.github.io/commits");
   
   if (!api.ok) throw new Error("[!] An error while getting commit from GitHub.");
 
   const commit = (await api.json())[0];
 
   const sha = commit.sha.slice(0, 7);
-  const date = commit.commit.author.date.slice(0, 10).replace(/-/g, '.');
+  const date = commit.commit.author.date.slice(0, 10).replace(/-/g, ".");
   
   document.getElementById("commit").innerText = `sha ${sha} · ${date} (UTC)`;
 });
