f70ce19cbccd7d1cafe5fcd99bbaf6ce42684478
Nikita Akumov <50026919+nubovik01@users.noreply.github.com>
2025-03-09 06:50:12 +0300

    remove unused line

--- files/js/footer.js
+++ files/js/footer.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const html = `
     <span class="line"></span>
-    <p id="quote" onclick="changeQuote();" translate="no"></p>
     <p id="commit" translate="no"></p>
     <p class="copyrights" translate="no">
       Created by qwkrtezzz not w/ <3<br>
