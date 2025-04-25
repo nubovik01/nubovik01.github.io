1c165da95c3a1d49a50a7a58cc703324b241d6e0
qwkrtezzz <hyperbola@kittyy.ru>
2025-04-25 21:02:59 +0300

    changes

--- files/js/footer.js
+++ files/js/footer.js
@@ -1,26 +1,32 @@
 // code by qwkrtezzz (https://github.com/nubovik01)
 // file created at 13 September 2024 21:02:55 PM (UTC+3)
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const footer = document.querySelector(".footer");
-
-  const html = `
+  footer.innerHTML = `
     <span class="line"></span>
     <p id="commit" translate="no"></p>
+    <p id="debug" style="display: none;" translate="no">
+      ———————————————————<br>
+      📐 ${screen.width}x${screen.height} (${window.innerWidth}x${window.innerHeight})<br>
+      💻 ${navigator.userAgent}<br>
+      ———————————————————
+    </p>
     <p class="copyrights" translate="no">
       Created by qwkrtezzz not w/ <3<br>
       © qwkrtezzz, <span class="year">2021</span><br>
     </p>
   `;
 
-  footer.innerHTML = html;
+  document.getElementById("commit").onclick = () => {
+    const debugElement = document.getElementById("debug");
+    debugElement.style.display = debugElement.style.display === "none" ? "block" : "none";
+  };
 
   document.querySelectorAll(".year").forEach(element => {
     const nowYear = new Date().getFullYear();
-    const copyrightYear = element.textContent;
-
-    if (nowYear != copyrightYear) {
-      element.innerText = copyrightYear + "-" + nowYear;
-    };
+    if (element.textContent != nowYear) {
+      element.innerText += `-${nowYear}`;
+    }
   });
 });
--- files/js/sha.js
+++ files/js/sha.js
@@ -1,7 +1,7 @@
 // code by qwkrtezzz (https://github.com/nubovik01)
 // file created at 17 February 2024 01:57:08 AM (UTC+3)
 
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener("DOMContentLoaded", async () => {
   const api = await fetch("https://api.github.com/repos/nubovik01/nubovik01.github.io/commits");
   
   if (!api.ok) throw new Error("[!] An error while getting commit from GitHub.");
