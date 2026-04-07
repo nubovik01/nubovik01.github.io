e13d64200841e544e6ffe05e9c002d85e05f90dd
Nikita Akumov <50026919+nubovik01@users.noreply.github.com>
2026-04-07 18:23:55 +0300

    changes!!!

--- files/js/birthdayTimer.js
+++ files/js/birthdayTimer.js
@@ -8,13 +8,24 @@ function updateBirthdayTimer() {
 
   if (now > birthday) birthday.setUTCFullYear(birthday.getUTCFullYear() + 1);
 
+  if (now.getUTCDate() === birthday.getUTCDate() && now.getUTCMonth() === birthday.getUTCMonth()) {
+    document.getElementById("birthday-timer").innerHTML = "<span>right now! 🎉🥳</span>";
+    return;
+  };
+
   const diff = birthday - now;
-  document.getElementById("birthday-timer").innerHTML = `
-    <span>${Math.floor(diff / 86400000)}d</span>
-    <span>${Math.floor((diff / 3600000) % 24)}h</span>
-    <span>${Math.floor((diff / 60000) % 60)}m</span>
-    <span>${Math.floor((diff / 1000) % 60)}s</span>
-  `;
+  const birthdayDays = Math.floor(diff / 86400000);
+  const birthdayHours = Math.floor((diff / 3600000) % 24);
+  const birthdayMinutes = Math.floor((diff / 60000) % 60);
+  const birthdaySeconds = Math.floor((diff / 1000) % 60);
+
+  let parts = [];
+  if (birthdayDays) parts.push(`<span>${birthdayDays}d</span>`);
+  if (birthdayHours) parts.push(`<span>${birthdayHours}h</span>`);
+  if (birthdayMinutes) parts.push(`<span>${birthdayMinutes}m</span>`);
+  if (birthdaySeconds) parts.push(`<span>${birthdaySeconds}s</span>`);
+
+  document.getElementById("birthday-timer").innerHTML = parts.join(" ");
 };
 
 document.addEventListener("DOMContentLoaded", () => {
