3eb95d990c4d2667f62d50379082fbebe8f0dc1f
Nikita Akumov <50026919+nubovik01@users.noreply.github.com>
2026-04-08 21:04:52 +0300

    fixes

--- files/js/birthdayTimer.js
+++ files/js/birthdayTimer.js
@@ -2,20 +2,20 @@
 // file created at 7 April 2026 05:31:49 PM (UTC+3)
 
 function updateBirthdayTimer() {
-  const now = new Date();
-  const moscowTime = new Date(now.toLocaleString("en-US", { timeZone: "Europe/Moscow" }));
+  const now = new Date(new Date().toLocaleString("en-US", { timeZone: "Europe/Moscow" }));
 
-  let birthday = new Date(moscowTime.getFullYear(), 3, 9);
+  let birthday = new Date(now.getFullYear(), 3, 9);
 
-  if (moscowTime > birthday) birthday.setFullYear(birthday.getFullYear() + 1);
+  if (now > birthday) birthday.setFullYear(birthday.getFullYear() + 1);
 
   if (
-    moscowTime.getDate() === birthday.getDate() && moscowTime.getMonth() === birthday.getMonth()
+    now.getDate() === birthday.getDate() &&
+    now.getMonth() === birthday.getMonth()
   ) {
     return document.getElementById("birthday-timer").innerHTML = "<span>right now! 🎉🥳</span>";
   };
 
-  const diff = birthday - moscowTime;
+  const diff = birthday - now;
 
   const birthdayDays = Math.floor(diff / 86400000);
   const birthdayHours = Math.floor((diff / 3600000) % 24);
