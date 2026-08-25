8b8a445bfcde89cad46c7890eeb19cb00d4cdd20
Nikita Akumov <50026919+nubovik01@users.noreply.github.com>
2026-08-25 05:54:41 +0300

    fix f*cked footer

--- files/js/footer.js
+++ files/js/footer.js
@@ -2,33 +2,35 @@
 // file created at 13 September 2024 21:02:55 PM (UTC+3)
 
 document.addEventListener("DOMContentLoaded", () => {
-  const footer = document.querySelector("footer");
-  footer.innerHTML = `
-    <span class="line"></span>
-    <p id="commit" translate="no"></p>
-    <p id="debug" style="display: none;" translate="no">
-      ———————————————————<br>
-      You found an easter egg!<br>
-      📐 ${screen.width}x${screen.height} (${window.innerWidth}x${window.innerHeight})<br>
-      💻 ${navigator.userAgent}<br>
-      ———————————————————
-    </p>
-    <p class="copyrights" translate="no">
-      Created by qwkrtezzz not w/ <3<br>
-      hyperbola[at]kittyy[dot]ru<br>
-      © qwkrtezzz, <span class="year">2021</span><br>
-    </p>
-  `;
+  document.querySelectorAll("footer").forEach(footer => {
+    footer.innerHTML = `
+      <span class="line"></span>
+      <p class="commit" translate="no"></p>
+      <p class="debug" style="display: none;" translate="no">
+        ———————————————————<br>
+        You found an easter egg!<br>
+        📐 ${screen.width}x${screen.height} (${window.innerWidth}x${window.innerHeight})<br>
+        💻 ${navigator.userAgent}<br>
+        ———————————————————
+      </p>
+      <p class="copyrights" translate="no">
+        Created by qwkrtezzz not w/ <3<br>
+        hyperbola[at]kittyy[dot]ru<br>
+        © qwkrtezzz, <span class="year">2021</span><br>
+      </p>
+    `;
 
-  document.getElementsByClassName("line")[0].onclick = () => {
-    const debugElement = document.getElementById("debug");
-    debugElement.style.display = debugElement.style.display === "none" ? "block" : "none";
-  };
-
-  document.querySelectorAll(".year").forEach(element => {
-    const nowYear = new Date().getFullYear();
-    if (element.textContent != nowYear) {
-      element.innerText += `-${nowYear}`;
+    footer.querySelector(".line").onclick = () => {
+      const debugElement = footer.querySelector(".debug");
+      debugElement.style.display = debugElement.style.display === "none" ? "block" : "none";
     };
+
+    footer.querySelectorAll(".year").forEach(element => {
+      const nowYear = new Date().getFullYear();
+
+      if (element.textContent != nowYear) {
+        element.innerText += `-${nowYear}`;
+      };
+    });
   });
 });
